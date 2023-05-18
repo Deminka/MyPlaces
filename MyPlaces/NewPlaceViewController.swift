06d18feb7fd716a5e8284f438f829313a1278d91
Dmitry Komissarov <komdimkom@gmail.com>
2023-05-18 12:43:33 +0300

    customization my place view

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by mac on 18.05.2023.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+    
+}
+
+// MARK: Text field delegate
+
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    //скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
